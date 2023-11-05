74b4f8ddd4004d2e6b0fc4806ba2ff31d8a47838
i-havr <i.havrylov@gmail.com>
2023-11-05 08:47:52 +0200

    first commit

---
+++ src/ormconfig.ts
@@ -0,0 +1,15 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 's_admin',
+  password: '123',
+  database: 's_db',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+};
+
+export default config;
