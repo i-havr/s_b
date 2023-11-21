9ca771388804b00fd7f125ddeb25975cc3278724
i-havr <i.havrylov@gmail.com>
2023-11-21 15:19:28 +0200

    edit cors settings

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // app.enableCors(); // ще один варіант, як включити корс
   await app.listen(process.env.PORT);
 }
 bootstrap();
